ef33122c0c16cef6fca759171316d29200993deb
Jamz Kuyazs <Jamz Kuyazs@DESKTOP-0QRJ192>
2023-03-06 17:56:59 +0800

    Add project files.

---
+++ Multiply/Program.cs
@@ -0,0 +1,30 @@
+﻿// Property of: James Crispe
+// Created on: Feb 20,2023
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 10;
+            int col = 10;
+
+            for(int a = 1; a <= row; a++) { 
+            for(int b = 1; b <= col; b++) {
+
+                    Console.Write(a * b + "\t");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
